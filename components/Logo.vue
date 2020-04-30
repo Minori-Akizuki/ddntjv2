65ee89495ff3fce8d93a85f377cae2447381218c
秋月みのり <minori@Spica.local>
2020-04-30 16:54:06 +0900

    first commit

---
+++ components/Logo.vue
@@ -0,0 +1,18 @@
+<template>
+  <img src="/ddntj_logo.svg">
+</template>
+<style>
+.NuxtLogo {
+  animation: 1s appear;
+  margin: auto;
+}
+
+@keyframes appear {
+  0% {
+    opacity: 0;
+  }
+  100% {
+    opacity: 1;
+  }
+}
+</style>
